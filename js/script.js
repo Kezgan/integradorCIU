475f2b6d8136140bc37769b6d44252554af231b0
Kezgan <lautaromcasella@gmail.com>
2020-06-19 01:17:50 -0300

    Login completo. Completando las demas paginas

--- js/script.js
+++ js/script.js
@@ -1,45 +1,33 @@
-var numeroDocumento = document.getElementById("dni")
-var valor = document.getElementById("dni").value
-var password = document.getElementById("password")
+var boton = document.getElementById("entrar");
+var inputDNI = document.getElementById("dni");
+var inputPassword = document.getElementById("password");
+var error = document.getElementById("error")
 
-function verificarDNI(valor) {
-
-    return /^\d{8}$/.test(valor)
-
-    /*key = e.keyCode || e.which
-
-    var teclado = String.fromCharCode(key)
-    var numeros = "0123456789"
-    var especiales = "8-37-38-46"
-    var tecladoEspecial = false
-
-    for(var i in especiales) {
-        if(key == especiales[i]) {
-            tecladoEspecial = true
-        }
-    }
-
-    if(numeros.indexOf(teclado) == -1 && !tecladoEspecial) {
-        return false
-    }*/
+function verificarDNI(documentoValor) {
+    return /^\d{8}$/.test(documentoValor)
 }
 
-function verificarPassword()
-{
-    var message = document.getElementById("errorPassword")
- 	
-    if(password.value.length >= 6) {
-        message.innerHTML = "La contraseña tiene mas de 6 dígitos!"
-    } else if(password.value == null) {
-        message.innerHTML = "Ingrese una contraseña válida"
+function verificarPassword(passwordValor) {
+    var letras = /[a-z]/g;
+    var numeros = /[0-9]/g;
+
+    return passwordValor.length >= 6 && passwordValor.match(numeros) && passwordValor.match(letras) 
+}
+
+function logear() {
+    var documentoValor = document.getElementById("dni").value;
+    var passwordValor = document.getElementById("password").value;
+
+    if(verificarDNI(documentoValor) && verificarPassword(passwordValor)) {
+        location.href = "home.html"
+    } else {
+        inputDNI.classList = "error"
+        inputPassword.classList = "error"
+        setTimeout('inputDNI.classList = "datos"' , 3000)
+        setTimeout('inputPassword.classList = "datos"' , 3000)
+        error.innerHTML = "Datos incorrectos, vuelva a ingresarlos."
     }
 }
 
-password.onkeypress = verificarPassword
-/*numeroDocumento.onkeypress = verificarDNI*/
 
-numeroDocumento.addEventListener("keypress" , () => {
-    if(verificarDNI(valor)) {
-    console.log("anda")
-    }
-})
+boton.addEventListener("click" , logear);
