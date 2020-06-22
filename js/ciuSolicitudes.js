dedd8657c50dee4f9ee3df0eaa54792d427dadf8
Kezgan <lautaromcasella@gmail.com>
2020-06-22 01:12:31 -0300

    Actualizado. Completando

--- js/ciuSolicitudes.js
+++ js/ciuSolicitudes.js
@@ -1,13 +1,14 @@
 var bEnviar = document.getElementById("bEnviar")
 
 function addDiv() {
-    var textArea = document.querySelector("textarea").value
+    var textArea = document.querySelector("textarea")
     var contenedor = document.createElement("DIV")
-    var texto = document.createTextNode(textArea)
+    var texto = document.createTextNode(textArea.value)
 
     contenedor.appendChild(texto)
     document.getElementById("historial").appendChild(contenedor)
     contenedor.classList = "contenedor"
+    textArea.value = ""
 }
 
 bEnviar.addEventListener("click" , addDiv)
