ea7ec86a69d1c7a9c51fa8b8e8186a1102a51a63
Kezgan <lautaromcasella@gmail.com>
2020-06-24 01:49:27 -0300

    Falta editar e iteracion 2. Completando

--- js/ciuSolicitudes.js
+++ js/ciuSolicitudes.js
@@ -1,4 +1,5 @@
 var bEnviar = document.getElementById("bEnviar")
+var bEliminar = document.getElementById("bEliminar")
 
 const xhttp = new XMLHttpRequest();
 
@@ -17,6 +18,7 @@ xhttp.onreadystatechange = function() {
                     <td>${i.descripcion}</td>
                     <td>${i.fechaSolicitud}</td>
                     <td>${i.estado}</td>
+                    <td><input type="checkbox" id="checkbox" name="input"></input></td>
                 </tr>
             `
         }
@@ -24,6 +26,7 @@ xhttp.onreadystatechange = function() {
 }
 
 function addDiv() {
+    /*
     var textArea = document.querySelector("textarea")
     var contenedor = document.createElement("DIV")
     var texto = document.createTextNode(textArea.value)
@@ -32,6 +35,46 @@ function addDiv() {
     document.getElementById("historial").appendChild(contenedor)
     contenedor.classList = "contenedor"
     textArea.value = ""
+    */
+
+   var textArea = document.querySelector("textarea")
+   var texto = document.querySelector("textArea").value
+   var fecha = new Date()
+
+   res.innerHTML += `
+        <tr>
+            <td>${texto}</td>
+            <td>${fecha.getDate() + "/" + (fecha.getMonth()+1) + "/" + fecha.getFullYear()}</td>
+            <td>Abierto</td>
+            <td><input type="checkbox" id="checkbox" name="input"></input></td>
+        </tr>
+    `
+    textArea.value = ""
 }
 
+function eliminarFila() {
+    var table = document.getElementById("tabla").tBodies[0];
+    var rowCount = table.rows.length;
+    var contador = 0
+
+    for(var i=0; i<rowCount; i++) {
+        var row = table.rows[i];
+        var chkbox = row.cells[3].getElementsByTagName('input')[0];
+        
+        if(chkbox.checked == true) {
+            table.deleteRow(i);
+            rowCount--;
+            i--;
+            contador ++;
+        }
+    }
+    
+    if(contador == 0) {
+        alert("No seleccionaste ninguna opcion")
+    }
+}
+
+
+
 bEnviar.addEventListener("click" , addDiv)
+bEliminar.addEventListener("click" , eliminarFila)
