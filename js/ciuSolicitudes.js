4a556412065fda48b73f78437ec9d0b906bec3ac
Kezgan <lautaromcasella@gmail.com>
2020-06-23 23:02:41 -0300

    Actualizado. Completando

--- js/ciuSolicitudes.js
+++ js/ciuSolicitudes.js
@@ -1,5 +1,28 @@
 var bEnviar = document.getElementById("bEnviar")
 
+const xhttp = new XMLHttpRequest();
+
+xhttp.open("GET", "js/datos.json", true);
+xhttp.send();
+xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        let datos = JSON.parse(this.responseText)
+        let res = document.getElementById("res")
+
+        res.innerHTML = ""
+
+        for(let i of datos) {
+            res.innerHTML += `
+                <tr>
+                    <td>${i.descripcion}</td>
+                    <td>${i.fechaSolicitud}</td>
+                    <td>${i.estado}</td>
+                </tr>
+            `
+        }
+    }
+}
+
 function addDiv() {
     var textArea = document.querySelector("textarea")
     var contenedor = document.createElement("DIV")
